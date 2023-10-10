a3f8582c67b175d3ac53c852c13db54e4dfbebdf
Yunfan Wu <wuyunfan15@mails.ucas.ac.cn>
2023-10-10 18:32:08 +0800

    Update login.py

--- login.py
+++ login.py
@@ -5,6 +5,7 @@ import os
 os.environ["PATH"] += os.pathsep + os.path.dirname(os.path.realpath(__file__))
 driver = webdriver.PhantomJS()
 driver.get('https://gw.ict.ac.cn/')
+time.sleep(2)
 driver.set_window_size(1124, 850)
 name = driver.find_element_by_id("username")
 name.send_keys('账号')
